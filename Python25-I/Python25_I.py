a00928098cd24b120e5931ea48fdd90b637c742b
StAland <steffen.aland@gmx.de>
2025-05-26 10:14:54 +0200

    Datentypen

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,1 +1,24 @@
+from xml.etree.ElementTree import tostring
 
+
+print("Hello world")
+print("Auch Hallo")
+
+variable1 = 3 #Integer
+variable2 = "Hello World" #string
+variable3 = 3.75 #float
+variable4 = 'v' #char
+variable5 = True #boolean
+
+ergebnis1 = 3+5
+ergebnis2 = 3>5
+ergebnis3 = variable1 == 3
+
+variable1 = 5
+variable1 = "Hallo"
+
+ergebnis4 = variable1
+
+
+
+print(ergebnis4)
