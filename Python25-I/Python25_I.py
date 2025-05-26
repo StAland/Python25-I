28a8ff238080d5d9b9c5f63bf3b662551e9c60fc
StAland <steffen.aland@gmx.de>
2025-05-26 13:45:50 +0200

    Input

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,41 +1,44 @@
-print("Hello world")
-print("Auch Hallo")
+# print("Hello world")
+# print("Auch Hallo")
 
-PI = 3.14 #Konstante wird aber nicht forciert
+# PI = 3.14 #Konstante wird aber nicht forciert
 
-variable1 = 3 #Integer
-variable2 = "Hello World" #string
-variable3 = 3.75 #float
-variable4 = 'v' #char
-variable5 = True #boolean
+# variable1 = 3 #Integer
+# variable2 = "Hello World" #string
+# variable3 = 3.75 #float
+# variable4 = 'v' #char
+# variable5 = True #boolean
 
-ergebnis1 = 3+5
-ergebnis2 = 3>5
-ergebnis3 = variable1 == 3
+# ergebnis1 = 3+5
+# ergebnis2 = 3>5
+# ergebnis3 = variable1 == 3
 
-variable1 = 5
-variable1 = "Hallo"
+# variable1 = 5
+# variable1 = "Hallo"
 
-ergebnis4 = variable1
+# ergebnis4 = variable1
 
-#Konvertierungen
+# #Konvertierungen
 
-string1 = "Hallo"
-int1 = 4
-kombinierter_string = string1 + str(int1)
+# string1 = "Hallo"
+# int1 = 4
+# kombinierter_string = string1 + str(int1)
 
-string2 = "2"
-int2 = 5
-ergebnis_mult = int2 * int(string2)
+# string2 = "2"
+# int2 = 5
+# ergebnis_mult = int2 * int(string2)
 
-#das funktioniert nicht
-#string3 = "2.15"
-#int3 = int(string3)
+# #das funktioniert nicht
+# #string3 = "2.15"
+# #int3 = int(string3)
 
-float4 = 2.15
-int4 = int(float4)
+# float4 = 2.15
+# int4 = int(float4)
 
-string5 = "2.15"
-float5 = float(string5)
+# string5 = "2.15"
+# float5 = float(string5)
 
-print(float5)
+# print(float5)
+
+name = input("Bitte gebe deinen Namen ein: ")
+print("Dein Name ist: " + name)
