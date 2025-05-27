7bd4d5c1c7c83b553d445c985afdd84ce72a245a
StAland <steffen.aland@gmx.de>
2025-05-27 12:33:07 +0200

    Schleifen

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,19 +1,36 @@
-def fakultaet(zahl):
-    if zahl <= 1:
-        return 1
-    ergebnis = zahl * fakultaet(zahl-1)
+def countdown(n):
+    while n >= 0:
+        print(n)
+        n -= 1
+        
+def countup(n):
+    for zahl in range(0, n+1, 1):   
+        print(zahl)
+
+def fac_iterativ(n):
+    ergebnis = 1
+    while n > 1:
+        ergebnis *= n
+        n -= 1
     return ergebnis
 
-def fibonacci(n):
-    if n <= 2:
-        return 1
-    print(n)
-    ergebnis = fibonacci(n-1) + fibonacci(n-2)
-    return ergebnis
+def array_ausgabe(n):
+    for element in n:
+        print(element)
+        
+def gib_Steffen_ein():
+    eingabe = ""
+    while eingabe != "Steffen":
+        eingabe = input("Gib Steffen ein: ")
 
 def main():
-    ergebnis = fakultaet(500)
-    print("Das Ergebnis ist: ", ergebnis)
+    #countdown(19)
+    #print(fac_iterativ(10))
+    #countup(15)
+    # ar = [1, 6, 9, 10, "Hallo", 3.15]
+    # ar[2] = 7
+    # array_ausgabe(ar)
+    gib_Steffen_ein()
 
 
 main()
