261bb55ad7c561598a3b610aa6f36e2b03ba87f3
StAland <steffen.aland@gmx.de>
2025-05-27 11:20:42 +0200

    fibonacci

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -4,9 +4,15 @@ def fakultaet(zahl):
     ergebnis = zahl * fakultaet(zahl-1)
     return ergebnis
 
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    print(n)
+    ergebnis = fibonacci(n-1) + fibonacci(n-2)
+    return ergebnis
 
 def main():
-    ergebnis = fakultaet(6)
+    ergebnis = fakultaet(500)
     print("Das Ergebnis ist: ", ergebnis)
 
 
