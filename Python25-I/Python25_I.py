a226252dc8d4a5bd2e0aa21b7a8470fb8361d064
StAland <steffen.aland@gmx.de>
2025-06-16 13:47:44 +0200

    Methoden, optionale Parameter

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,44 +1,30 @@
 def main():
-    # haus1 = Haus(6, "gelb", 2)
-    # print("Farbe: ", haus1.farbe)    
-
-    # print("Stockwerke: ", haus1.get_stockwerke())
-    # haus1.set_stockwerke("Hallo")
-    # print("Stockwerke: ", haus1.get_stockwerke())
-    # haus1.set_stockwerke(3)
-    # print("Stockwerke: ", haus1.get_stockwerke())
+    einkaufsliste = Einkaufsliste()
+    einkaufsliste.artikel_hinzufuegen("Butter", 2)
+    einkaufsliste.artikel_hinzufuegen("Honig", 1)
+    einkaufsliste.artikel_entfernen("Butter")
+    einkaufsliste.ausgabe()
     
-    auto = Auto("Mazda 2", "gruen")
-    print(auto.get_model(), " ", auto.get_farbe())
+class Einkaufsliste:
     
-class Auto:
-    def __init__(self, model, farbe):
-        self.__model = model
-        self.__farbe = farbe
-    
-    def get_model(self):
-        return self.__model
-    
-    def get_farbe(self):
-        return self.__farbe
-    
-    def set_farbe(self, value):
-        self.__farbe = value
-
-class Haus:
-    def __init__(self, hoehe, farbe, stockwerke):
-        self.hoehe = hoehe
-        self.farbe = farbe
-        if type(stockwerke) == int:
-            self.__stockwerke = stockwerke
+    def __init__(self, vorhandenesDictionary = {}):
+        self.__liste = vorhandenesDictionary
+        
+    def artikel_hinzufuegen(self, artikel, anzahl):
+        if artikel in self.__liste:
+            self.__liste[artikel] += anzahl
         else:
-            self.__stockwerke = 1
+            self.__liste[artikel] = anzahl
             
-    def get_stockwerke(self):
-        return self.__stockwerke
-    
-    def set_stockwerke(self, value):
-        if type(value) == int:
-            self.__stockwerke = value
+    def ausgabe(self):
+        for artikel, anzahl in self.__liste.items():
+            print(artikel, anzahl)
+            
+    def artikel_entfernen(self, artikel, anzahl = 1):
+        if artikel in self.__liste:
+            if self.__liste[artikel] > anzahl:
+                self.__liste[artikel] -= anzahl
+            else:
+                del self.__liste[artikel]
 
 main()
