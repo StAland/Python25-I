ff857b205933c9bd01448d4060a4478bbede3967
StAland <steffen.aland@gmx.de>
2025-06-16 09:25:53 +0200

    Aufgabe 4

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,11 +1,19 @@
 def main():
-    notenliste = [1.7, 2.3, 1.0, 3.0]
-    print(notenschnitt(notenliste))
-
-def notenschnitt(notenliste):
-    summe = sum(notenliste)
-    anzahl = len(notenliste)
+    satz = "Hallo Welt Hallo"
+    wort_haufigkeit(satz)
+    satz2 = "Test TEst Test TeSt"
+    wort_haufigkeit(satz2)
     
-    return summe / anzahl
+def wort_haufigkeit(text):
+    woerter = text.split()
+    haeufigkeit = {}
+
+    for wort in woerter:
+        if wort in haeufigkeit:
+            haeufigkeit[wort] += 1
+        else:
+            haeufigkeit[wort] = 1
+    return haeufigkeit
+
 
 main()
