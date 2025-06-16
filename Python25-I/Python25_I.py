c8f3e055130ba1a945b6b7b7bf0e0c15858283a4
StAland <steffen.aland@gmx.de>
2025-06-16 09:12:30 +0200

    Aufgabe 1 Python Array Dictionary

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,30 +1,11 @@
-
-
-
 def main():
-    text = "hallo"
-    print(text.capitalize())
-    print(text)
-    text = "HaLLo"
-    print(text.casefold()) #Seite 180 Fehler bei casefold der erste Buchstabe bleibt nicht gross
-    print(text.center(10, "*")) #Wenn eine ungerade Anzahl an Fuellzeichen benutzt wird, wir hinten eins mehr verwendet
-    print(text.count("L"))
-    print(text.count("L",3,5))
-    print(text.find("L"))
-    print(text.find("l"))   #nicht gefunden gibt -1
-    print(text.index("L"))
-    #print(text.index("l"))  #nicht gefunden gibt einen Fehler
-    print(text.rfind("L"))
-    print(text.rindex("L"))
-    print(text.isalpha())
-    text2 = "Wasser02"
-    print(text2.isalpha())
-    text3 = "12.3423"
-    print(text3.isdecimal())
-    print(text.lower())
-    print(text.upper())
-    print(text.replace("L", "l"))
+    notenliste = [1.7, 2.3, 1.0, 3.0]
+    print(notenschnitt(notenliste))
+
+def notenschnitt(notenliste):
+    summe = sum(notenliste)
+    anzahl = len(notenliste)
     
-    
+    return summe / anzahl
 
 main()
