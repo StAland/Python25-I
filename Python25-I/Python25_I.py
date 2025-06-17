873ebc51c71dc94912f5c7c0866ea7939e33eee8
StAland <steffen.aland@gmx.de>
2025-06-17 08:12:07 +0200

    Python Objektorientierung Aufgabe 2

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,30 +1,23 @@
 def main():
-    einkaufsliste = Einkaufsliste()
-    einkaufsliste.artikel_hinzufuegen("Butter", 2)
-    einkaufsliste.artikel_hinzufuegen("Honig", 1)
-    einkaufsliste.artikel_entfernen("Butter")
-    einkaufsliste.ausgabe()
+    hund = Haustier("Doggie", "Hund")
+    hund.set_name(12)
+    print(hund.get_name())
+    hund.set_name("Chani")
+    print(hund.get_name())
     
-class Einkaufsliste:
-    
-    def __init__(self, vorhandenesDictionary = {}):
-        self.__liste = vorhandenesDictionary
+class Haustier:
+    def __init__(self, name, art):
+        self.__name = name
+        self.__art = art
         
-    def artikel_hinzufuegen(self, artikel, anzahl):
-        if artikel in self.__liste:
-            self.__liste[artikel] += anzahl
-        else:
-            self.__liste[artikel] = anzahl
-            
-    def ausgabe(self):
-        for artikel, anzahl in self.__liste.items():
-            print(artikel, anzahl)
-            
-    def artikel_entfernen(self, artikel, anzahl = 1):
-        if artikel in self.__liste:
-            if self.__liste[artikel] > anzahl:
-                self.__liste[artikel] -= anzahl
-            else:
-                del self.__liste[artikel]
+    def get_name(self):
+        return self.__name
+    
+    def get_art(self):
+        return self.__art
+    
+    def set_name(self, neuer_name):
+        if type(neuer_name) == str and neuer_name != "":
+            self.__name = neuer_name
 
 main()
