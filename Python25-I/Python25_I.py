1f744db9aa3b3fd25ef2de663a1cbce37a4da4b0
OutdoorRob <123952874+OutdoorRob@users.noreply.github.com>
2025-06-17 15:00:45 +0200

    Vererbung: Beispiel Autos mit Vererbung

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -8,47 +8,47 @@
 
 # - - - Definitionen - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-class VerbrennerAuto:
-    def __init__(self, hersteller, modell, farbe, kraftstoff):
+class Auto:
+    def __init__(self, hersteller, modell, farbe):
         self.__hersteller = hersteller
         self.__modell = modell
         self.__farbe = farbe
-        self.__kraftstoff = kraftstoff
     
     def set_farbe(self, value):
         if type(value) == str:
             self.__farbe = value
         else:
             print("Keine gültige Farbe!")
+    
+    def __str__(self):
+        text = self.__hersteller + " " + self.__modell + " | "
+        text += self.__farbe + " | "
+        return text
+
+
+class VerbrennerAuto(Auto):
+    def __init__(self, hersteller, modell, farbe, kraftstoff):
+        super().__init__(hersteller, modell, farbe)
+        self.__kraftstoff = kraftstoff
     
     def get_kraftstoff(self):
         return self.__kraftstoff
 
     def __str__(self):
-        text = self.__hersteller + " " + self.__modell + " | "
-        text += self.__farbe + " | "
+        text = super().__str__()
         text += self.get_kraftstoff()
         return text
 
-class ElektroAuto:
+class ElektroAuto(Auto):
     def __init__(self, hersteller, modell, farbe, batterie_kWh):
-        self.__hersteller = hersteller
-        self.__modell = modell
-        self.__farbe = farbe
+        super().__init__(hersteller, modell, farbe)
         self.__batterie_kWh = batterie_kWh
-    
-    def set_farbe(self, value):
-        if type(value) == str:
-            self.__farbe = value
-        else:
-            print("Keine gültige Farbe!")
     
     def get_batterie_kWh(self):
         return self.__batterie_kWh
 
     def __str__(self):
-        text = self.__hersteller + " " + self.__modell + " | "
-        text += self.__farbe + " | "
+        text = super().__str__()
         text += str(self.get_batterie_kWh()) + " kWh"
         return text
 
