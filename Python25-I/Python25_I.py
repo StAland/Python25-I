7f9ad2335c304deb6939c3d2b4e891b3e2a6160a
StAland <steffen.aland@gmx.de>
2025-06-17 09:44:12 +0200

    Komposition

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -1,23 +1,37 @@
 def main():
-    hund = Haustier("Doggie", "Hund")
-    hund.set_name(12)
-    print(hund.get_name())
-    hund.set_name("Chani")
-    print(hund.get_name())
+    haus = Mietshaus()
+    wohnung1 = Wohnung(1000)
+    wohnung2 = Wohnung(840)
+    haus.add_wohnung(wohnung1)
+    haus.add_wohnung(wohnung2)
+    haus.add_wohnungspreis(1200)
+    print(haus.get_Gesamtmiete())
     
-class Haustier:
-    def __init__(self, name, art):
-        self.__name = name
-        self.__art = art
+class Mietshaus:
+    
+    def __init__(self):
+        self.__wohnungsliste = []
         
-    def get_name(self):
-        return self.__name
+    def add_wohnung(self, wohnung):
+        if type(wohnung) == Wohnung:
+            self.__wohnungsliste.append(wohnung)
+            
+    def add_wohnungspreis(self, preis):
+        if type(preis) == int:
+            self.__wohnungsliste.append(Wohnung(preis))
+            
+    def get_Gesamtmiete(self):
+        summe = 0
+        for wohnung in self.__wohnungsliste:
+            summe += wohnung.get_miete()
+        return summe
+        
+class Wohnung:
     
-    def get_art(self):
-        return self.__art
-    
-    def set_name(self, neuer_name):
-        if type(neuer_name) == str and neuer_name != "":
-            self.__name = neuer_name
+    def __init__(self, miete):
+        self.__miete = miete
+        
+    def get_miete(self):
+        return self.__miete
 
 main()
