ef1e8fb5a322e5a30358c6e7efa6d948b85887c8
OutdoorRob <123952874+OutdoorRob@users.noreply.github.com>
2025-06-18 09:20:31 +0200

    Vererbung: Nachtrag zu Konstruktoren / Attributen

--- Python25-I/Python25_I.py
+++ Python25-I/Python25_I.py
@@ -13,49 +13,50 @@ from abc import ABC, abstractmethod
 # - erbt von ABC
 # - eine abstrakte Methode enthält (@abstractmethod)
 class Tier(ABC):
-    def __init__(self, farbe):
+    @abstractmethod
+    def __init__(self, farbe, alter):
         self.__farbe = farbe
+        self.__alter = alter
 
     @abstractmethod
     def mach_geraeusch(self):
         print("Das Tier macht Geräusche.")
     
     def hallo(self):
-        print("Hallo, ich bin ein Tier.")
+        print("Hallo, ich bin", self.__farbe, "und", self.__alter, "Jahre alt.")
 
 # 'Hund' ist eine reguläre Klasse.
 # Da diese von der abstrakten 'Tier' erbt, muss 'Hund' die
 # abstrakte Methode 'mach_geraeusch()' implmentieren.
 class Hund(Tier):
+    def __init__(self, farbe, alter):
+        super().__init__(farbe, alter)
+
     def mach_geraeusch(self):
         super().mach_geraeusch()
         print("Der Hund bellt.")
 
-class Katze(Tier):
-    def mach_geraeusch(self):
-        print("Die Katze miaut.")
-    
-    def hallo(self):
-        print("Hallo, ich bin eine Katze.")
+# Der 'SchwarzeHund' ist eine reguläre Klasse und erbt alles von 'Hund' und 'Tier'.
+# Da die Farbe durch den Namen der Klasse quasi schon vorgegeben ist,
+# implemtieren wir hier einen neuen Konstruktor der nur noch das Alter akzeptiert.
+# Beim Aufruf des Konstrukturs der Eltern-Klasse 'Hund' (super().__init()) wird 
+# für die Farbe fest der Wert 'schwarz' übergeben und das Alter weitergeleitet.
+class SchwarzerHund(Hund):
+    def __init__(self, alter):
+        super().__init__("schwarz", alter)
 
-class Maus(Tier):
-    def mach_geraeusch(self):
-        print("Die Maus piept.")
 
 # - - - Test-Code / Demonstration  - - - - - - - - - - - - - - - - - - - - - - -
 
 def main():
     # tier = Tier()     # wirft einen Fehler, da Instanzen von abstrakten Klassen nicht erlaubt sind
-    hund = Hund("weiß")
-    katze = Katze("schwarz")
-    maus = Maus("grau")
+    hund = Hund("weiß", 7)
+    schwarzer_hund = SchwarzerHund(2)
 
-    hund.mach_geraeusch()
     hund.hallo()
-    
-    katze.mach_geraeusch()
-    katze.hallo()
+    hund.mach_geraeusch()
 
-    maus.mach_geraeusch()
+    schwarzer_hund.hallo()
+    schwarzer_hund.mach_geraeusch()
 
 main()
